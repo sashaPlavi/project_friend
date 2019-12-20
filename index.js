2c666ab5bd8943a2267280b63484db7b4f1d657c
SashaPlavi <sasha.bingulac@gmail.com>
2019-12-20 16:14:39 +0100

    rendering first page

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
-const paht = require("path");
+const path = require("path");
+const rootdir = require("./util/path");
+const year = new Date().getFullYear();
 
 const app = express();
+app.set("view engine", "ejs");
+app.use(express.static(path.join(rootdir, "public")));
 app.use((req, res, next) => {
-  res.sendFile(paht.join(__dirname, "view", "404.html"));
+  res.status(404).render("404.ejs", { pagetitle: "page not found", year });
 });
 
 app.listen(3001);
